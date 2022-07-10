319bfe4a9bffbf27a4c492fd1d0b5207ba0c973b
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2022-07-10 11:40:10 +0300

    Fixed connection string , migrated db

--- WATweetBook/Migrations/20220710070227_InitialMigration1.Designer.cs
+++ WATweetBook/Migrations/20220710070227_InitialMigration1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WATweetBook.Data;
 
@@ -11,9 +12,10 @@ using WATweetBook.Data;
 namespace WATweetBook.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220710070227_InitialMigration1")]
+    partial class InitialMigration1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
